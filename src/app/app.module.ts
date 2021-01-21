cb1cfae6befbcbba997ef949d6fb87eae7e0448d
Tony <tony1.bellec@epitech.eu>
2021-01-21 18:01:58 +0100

    Add httpclient module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ChallengeComponent } from './components/exploration/challenge/challenge.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { ChallengeComponent } from './components/exploration/challenge/challenge
     ChallengeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
