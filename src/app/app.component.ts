2e9664395ada95ada26d3a45329fe2b57386c261
Quentin <quentin2.quero@epitech.eu>
2021-01-22 11:32:54 +0100

    add character list and cors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blackstone-front-web';
+  static url = 'http://localhost:3000';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,22 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { Character_pageComponent } from './pages/character/character_page.component';
 import { CharacterDetailsComponent } from './components/character-details/character-details.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CharacterListPageComponent } from './pages/character-list-page/character-list-page.component';
+import {CharacterCardComponent} from "./components/character-card/character-card.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     Character_pageComponent,
-    CharacterDetailsComponent
+    CharacterDetailsComponent,
+    CharacterCardComponent,
+    CharacterListPageComponent
   ],
   imports: [
     BrowserModule,
-      AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
