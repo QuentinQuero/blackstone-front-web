d22954b14f6ce6bdf83a3043080cc3ebd945cee0
Quentin <quentin2.quero@epitech.eu>
2021-01-22 10:25:07 +0100

    création architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { AppRoutingModule } from './app-routing.module'; // CLI imports AppRouti
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CharacterComponent } from './character/character.component';
+import { Character_pageComponent } from './pages/character/character_page.component';
+import { CharacterDetailsComponent } from './components/character-details/character-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CharacterComponent
+    Character_pageComponent,
+    CharacterDetailsComponent
   ],
   imports: [
     BrowserModule,
