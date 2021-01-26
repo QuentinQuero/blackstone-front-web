7a17c4350adf75ede994973a2d8c3b0f644b4e45
Tony <tony1.bellec@epitech.eu>
2021-01-26 15:26:13 +0100

    Exploration page V1 OK

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { Character_pageComponent } from './pages/character/character_page.component';
 import {CharacterListPageComponent} from "./pages/character-list-page/character-list-page.component";
 import {ChallengesComponent} from "./pages/exploration/challenges/challenges.component";
+import { ExplorationComponent } from "./pages/exploration/exploration.component";
 
 /* importer vos pages ici */
 const routes: Routes = [
   { path: 'character', component: Character_pageComponent },
   { path: 'characters', component: CharacterListPageComponent },
-  { path: 'challenges', component: ChallengesComponent }
+  { path: 'challenges', component: ChallengesComponent },
+  { path: 'exploration', component: ExplorationComponent }
+
   /* créer vos path ici avec votre component ici */
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module'; // CLI imports AppRoutingModule
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import { Character_pageComponent } from './pages/character/character_page.component';
 import { CharacterDetailsComponent } from './components/character-details/character-details.component';
 import { CharacterListPageComponent } from './pages/character-list-page/character-list-page.component';
-import {CharacterCardComponent} from "./components/character-card/character-card.component";
+import { CharacterCardComponent } from './components/character-card/character-card.component';
+
+import { ExplorationComponent } from './pages/exploration/exploration.component';
 
 import { ChallengeCardsComponent } from './components/exploration/challenge-cards/challenge-cards.component';
-import {ChallengesComponent} from "./pages/exploration/challenges/challenges.component";
-
-import {HttpClientModule} from "@angular/common/http";
+import { ChallengesComponent } from './pages/exploration/challenges/challenges.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import {HttpClientModule} from "@angular/common/http";
     CharacterCardComponent,
     CharacterListPageComponent,
     ChallengeCardsComponent,
-    ChallengesComponent
+    ChallengesComponent,
+    ExplorationComponent
   ],
   imports: [
     BrowserModule,
