80a8dc24a9a60b00b77b1f5481fc493a1c76548a
Roma <roman.fechtchenko@epitech.eu>
2021-01-28 10:56:04 +0100

    fixed component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Character_pageComponent } from './pages/character/character_page.component';
-import { ExplorationComponent } from "./pages/exploration/exploration.component";
+import { ExplorationComponent } from './pages/exploration/exploration.component';
 import { MapComponent } from './components/exploration/map/map.component';
+import { ChallengesComponent} from './pages/exploration/challenges/challenges.component';
+import {CharacterListPageComponent} from './pages/character-list-page/character-list-page.component';
 
 /* importer vos pages ici */
 const routes: Routes = [
   { path: 'character_detail/:id', component: Character_pageComponent },
   { path: 'characters', component: CharacterListPageComponent },
-  { path: 'combatmaps', component: MapComponent }
+  { path: 'combatmaps', component: MapComponent },
   { path: 'challenges', component: ChallengesComponent },
   { path: 'exploration', component: ExplorationComponent }
   /* créer vos path ici avec votre component ici */
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module'; // CLI imports AppRoutingModule
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { Character_pageComponent } from './pages/character/character_page.component';
@@ -10,7 +10,6 @@ import { CharacterDetailsComponent } from './components/character-details/charac
 import { MapComponent } from './components/exploration/map/map.component';
 import { MapPageComponent } from './pages/exploration/map-page/map-page.component';
 
-import { HttpClientModule } from '@angular/common/http';
 import { CharacterListPageComponent } from './pages/character-list-page/character-list-page.component';
 import { CharacterCardComponent } from './components/character-card/character-card.component';
 
